f75022bf970569655600af71d218f2363a0ff45a
Emilio Dorigatti <emilio.dorigatti@boehringer-ingelheim.com>
2023-12-19 18:02:14 +0100

    part 2 day 19/2023

--- 2023/day19.jl
+++ 2023/day19.jl
@@ -126,6 +126,125 @@ function solve_part1(input)
 end
 
 
+const TRange = Dict{AbstractString, Tuple{Int, Int}}
+
+function slice_range(ru::Rule, rng::TRange, matching::Bool)::TRange
+    new_rng = TRange()
+    for k in keys(rng)
+        if k != ru.key
+            new_rng[k] = rng[k]
+        else
+            a, b = rng[k]
+            if matching
+                if ru.larger_than
+                    new_rng[k] = (ru.value + 1, b)
+                else
+                    new_rng[k] = (a, ru.value - 1)
+                end
+            else
+                if ru.larger_than
+                    new_rng[k] = (a, ru.value)
+                else
+                    new_rng[k] = (ru.value, b)
+                end
+            end
+        end
+    end
+    return new_rng
+end
+
+
+function count_parts(rng::TRange)::Int
+    r = 1
+    for (a, b) in values(rng)
+        if a <= b
+            r *= (b - a + 1)
+        else
+            r *= 0
+        end
+    end
+    return r
+end
+
+
+function solve_part2(input, sanity_checks = false)
+    workflows, _ = parse_input(input)
+
+    frontier = Vector{Tuple{AbstractString, TRange}}([
+        ("in", TRange(
+            "x" => (1, 4000),
+            "m" => (1, 4000),
+            "a" => (1, 4000),
+            "s" => (1, 4000)),
+        )
+    ])
+
+    count_accepted = count_rejected = 0
+    while !isempty(frontier)
+        if sanity_checks
+            total = 0 
+            for (node, rng) in frontier
+                total += count_parts(rng)
+            end
+            if total + count_accepted + count_rejected != 4000^4
+                error("???")
+            end
+        end
+
+        node, rng = pop!(frontier)
+        rng_fail = deepcopy(rng)
+
+        wo = workflows[node]
+        for ru in wo.rules
+            rng_match = slice_range(ru, rng_fail, true)
+            if any(a > b for (a, b) in values(rng_match))
+                # range is empty / no parts match this rule
+                continue
+            end
+
+            rng_fail = slice_range(ru, rng_fail, false)
+
+            if ru.destination == "A"
+                n = count_parts(rng_match)
+                count_accepted += n
+                #println("AAA from $node ($n parts from $new_rng with path $path)")
+            elseif ru.destination == "R"
+                n = count_parts(rng_match)
+                count_rejected += n
+                #println("RRR from $node ($n parts from $new_rng with path $path)")
+            else
+                push!(frontier, (ru.destination, rng_match))
+            end
+        end
+        
+        if any(a > b for (a, b) in values(rng_fail))
+            # range is empty
+            continue
+        end
+
+        if wo.default == "A"
+            n = count_parts(rng_fail)
+            count_accepted += n
+            #println("AAA from $node ($n parts from $rng_fail with path $path)")
+        elseif wo.default == "R"
+            n = count_parts(rng_fail)
+            count_rejected += n
+            #println("RRR from $node ($n parts from $rng_fail with path $path)")
+        else
+            push!(frontier, (wo.default, rng_fail))
+        end
+    end
+
+    println("rules accepted $count_accepted")
+    println("rules rejected $count_rejected")
+    println("total rules counted $(count_accepted+count_rejected) (expecting $(4000^4))")
+
+    return count_accepted
+end
+
+
 @assert 19114 == solve_file("2023/inputs/day19-test.txt", false)
 @assert 418498 == solve_file("2023/inputs/day19.txt", false)
 
+@assert 167409079868000 ==  solve_file("2023/inputs/day19-test.txt", true)
+@assert 123331556462603 == solve_file("2023/inputs/day19.txt", true)
